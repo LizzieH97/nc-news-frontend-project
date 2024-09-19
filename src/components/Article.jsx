82150695b86d2f2ee574f1224dbcb261df657841
LizzieH97 <erhughes97@gmail.com#>
2024-09-19 10:46:13 +0100

    can now vote on article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,28 +1,26 @@
 import { useState } from "react";
-
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getCommentByArticleID, getSingleArticle } from "../api/APICalls";
+import { getSingleArticle, upVoteArticle } from "../api/APICalls";
+import Comments from "./Comments";
 
 export default function Article() {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
-  const [comments, setComments] = useState([]);
-
+  const [vote, setVote] = useState(0);
   useEffect(() => {
     getSingleArticle(article_id).then((response) => {
       setArticle(response);
     });
   }, [article_id]);
-
-  useEffect(() => {
-    getCommentByArticleID(article_id).then(
-      (response) => {
-        setComments(response);
-      },
-      [article_id]
-    );
-  });
+  const handleUp = () => {
+    setVote((current_vote) => current_vote + 1);
+    upVoteArticle(article_id, 1).then((response) => {});
+  };
+  const handleDown = () => {
+    setVote((current_vote) => current_vote - 1);
+    upVoteArticle(article_id, -1).then((response) => {});
+  };
   return (
     <div className="single-article">
       <p>Topic: {article.topic}</p>
@@ -30,19 +28,10 @@ export default function Article() {
       <img src={article.article_img_url} className="article-img" />
       <h3>Written by: {article.author}</h3>
       <p>{article.body}</p>
-      <div className="comment-grid-container">
-        {comments.map((comment) => {
-          return (
-            <ul key={comment.comment_id} className="comment-grid-item">
-              <li className="comment-body">{comment.body} </li>
-              <li>
-                By: {comment.author} <br />
-                Votes: {comment.votes}
-              </li>
-            </ul>
-          );
-        })}
-      </div>
+      <p>Votes: {vote + article.votes}</p>
+      <button onClick={handleUp}>Wow!</button>
+      <button onClick={handleDown}>No!</button>
+      <Comments value={article} />
     </div>
   );
 }
